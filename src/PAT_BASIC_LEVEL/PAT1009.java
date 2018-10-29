2bc68785bbedc28d5b400fbc1626a96f47cb28a1
zephyrZQF <527941789@qq.com>
2018-10-29 16:40:17 +0800

    update

---
+++ src/PAT_BASIC_LEVEL/PAT1009.java
@@ -0,0 +1,21 @@
+package PAT_BASIC_LEVEL;
+
+import java.util.Scanner;
+
+public class PAT1009 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+        in.close();
+
+        String reve = new StringBuilder(str).reverse().toString();
+        String[] arrReve = reve.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < arrReve.length - 1; i ++){
+            sb.append(new StringBuilder(arrReve[i]).reverse());
+            sb.append(" ");
+        }
+        sb.append(new StringBuilder(arrReve[arrReve.length - 1]).reverse());
+        System.out.println(sb);
+    }
+}
