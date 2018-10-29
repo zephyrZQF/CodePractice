838d028ad5458a7024414d46062ffdc8d8cf4676
zephyrZQF <527941789@qq.com>
2018-10-29 17:44:33 +0800

    update

---
+++ src/PAT_BASIC_LEVEL/PAT1010.java
@@ -0,0 +1,31 @@
+package PAT_BASIC_LEVEL;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class PAT1010 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in );
+        String[] arr = in.nextLine().split("\\s+");  //这边有个坑，数字间的空格不止一个
+        int[] arrs = new int[arr.length];
+        for(int i = 0; i < arrs.length; i ++){
+            arrs[i] = Integer.parseInt(arr[i]);
+        }
+
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 0; i < arrs.length / 2; i++){
+            if(arrs[i*2] != 0 && arrs[i*2 + 1] != 0){
+                list.add(arrs[i*2]*arrs[i*2 + 1]);
+                list.add(arrs[i*2 + 1] - 1);
+            }
+        }
+
+        if(list.isEmpty()){
+            System.out.println("0 0");
+        }else {
+            System.out.println( list.toString().replaceAll("\\[|]|,",""));
+        }
+
+    }
+}
