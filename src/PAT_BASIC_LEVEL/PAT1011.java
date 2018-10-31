871687cd884e34a4ffa1f2c49bea68725a66fec1
zephyrZQF <527941789@qq.com>
2018-10-31 11:37:48 +0800

    update

--- src/PAT_BASIC_LEVEL/PAT1011.java
+++ src/PAT_BASIC_LEVEL/PAT1011.java
@@ -1,5 +1,10 @@
 package PAT_BASIC_LEVEL;
 
+
+/**
+ * 给定区间 [−2^31,2^31] 内的 3 个整数 A、B 和 C，请判断 A+B 是否大于 C。
+ */
+
 import java.util.Scanner;
 public class PAT1011{
     public static void main(String args[]){
