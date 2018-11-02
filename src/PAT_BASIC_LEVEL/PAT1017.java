e2f87eacf9e9d7f74ffcaf6eb57e046c50d26eb0
zephyrZQF <527941789@qq.com>
2018-11-02 11:45:34 +0800

    update

--- src/PAT_BASIC_LEVEL/PAT1017.java
+++ src/PAT_BASIC_LEVEL/PAT1017.java
@@ -1,32 +1,37 @@
 package PAT_BASIC_LEVEL;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 /**
- * A除以B
- * 本题要求计算 A/B，其中 A 是不超过 1000 位的正整数，B 是 1 位正整数。你需要输出商数 Q 和余数 R，使得 A=B×Q+R 成立。
- * 运行一直超时，换C++ 实现之后就成了
+ *本题要求计算 A/B，其中 A 是不超过 1000 位的正整数，B 是 1 位正整数。你需要输出商数 Q 和余数 R，使得 A=B×Q+R 成立。
+ *
+ * 总是有超时 ， 改成C++直接就通过了
  */
 
 public class PAT1017 {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String bigone = in.next();
-        int division = in.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = in.readLine().split("\\s+");
 
+        String bigone = input[0];
+        int division = Integer.parseInt(input[1]);
+        StringBuilder sb = new StringBuilder();
         int tmp = (bigone.charAt(0) - '0');
         if(  tmp/division != 0 ){
-            System.out.print(tmp/division);
+            sb.append(tmp/division);
         }
         int ans2 = tmp % division;
         for(int i = 1; i < bigone.length(); i ++){
             tmp = bigone.charAt(i) - '0';
             tmp = ans2*10 + tmp;
-            System.out.print(tmp/division);
+            sb.append(tmp/division);
             ans2 = tmp % division;
         }
 
-        System.out.println(" " + ans2);
+        System.out.println(sb + " " + ans2);
 
     }
 }
+
