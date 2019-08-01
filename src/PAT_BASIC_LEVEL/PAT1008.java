d3643eee3e51dce72d77aca1ae3f58acf5b27a8a
zephyrZQF <527941789@qq.com>
2019-08-02 00:51:46 +0800

    update

---
+++ src/PAT_BASIC_LEVEL/PAT1008.java
@@ -0,0 +1,24 @@
+package PAT_BASIC_LEVEL;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class PAT1008 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        int prefix = in.nextInt();
+        prefix %= num;
+        int[] arr = new int[num];
+        for(int i = 0, k = 0; i < num ; i ++){
+            if( i + prefix < num){
+                arr[ i + prefix ] = in.nextInt();
+            }else {
+                arr[ k++ ] = in.nextInt();
+            }
+        }
+
+        System.out.println(Arrays.toString(arr).replaceAll("\\[|\\]|\\,", ""));
+    }
+
+}
